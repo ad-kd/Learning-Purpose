7d79463e29e48fb6a5c121e0ab70288c295177d2
ad-kd <12218adhithyaksnm@gmail.com>
2025-12-09 19:44:13 +0530

    upgrade hero.jsx

--- src/components/home-component/hero.jsx
+++ src/components/home-component/hero.jsx
@@ -1,91 +1,84 @@
 import React from 'react'
-import { useRef } from 'react';
-import VariableProximity from '../rebits/VariableProximity.jsx';
-import LightRays from '../rebits/LightRays.jsx';
 import TextType from '../rebits/TextType.jsx';
-import ClickSpark from '../rebits/ClickSpark.jsx';
-
-
 
 const hero = () => {
-    const description = `
-Company-IT is transforming the IT sector by solving the costly and
- complex problem of AI using our proprietary Python. 
- This unique solution has already generated 98%, demonstrating 
- immense value and a clear path to dominating the $60 Billion global market opportunity. We offer 
- foreign investors a strategic entry point into a high-growth, resilient tech venture with a projected 
- Strategy. Join us in our Org
-to accelerate our international expansion and unlock significant mutual returns.`;
+  return (
+    <div>
 
-    const containerRef = useRef(null);
-    return (
-        <div >
-
-<ClickSpark
-  sparkColor='#fff'
-  sparkSize={10}
-  sparkRadius={15}
-  sparkCount={8}
-  duration={400}
->
-     <div style={{ width: '100%', height: '600px', position: 'relative', overflow: 'hidden', zIndex: -1 }}>
-
-                <LightRays
-                    raysOrigin="top-left"
-                    raysColor="#ffffff"
-                    raysSpeed={1.5}
-                    lightSpread={0.8}
-                    rayLength={1.2}
-                    followMouse={true}
-                    mouseInfluence={0.1}
-                    noiseAmount={0.1}
-                    distortion={0.05}
-                    className="bkclr"
-                />
-
-                <div className="hero-text ">
-
-                    <TextType className=' text-2xl md:text-6xl font-bold text-white'
-                        text={["Welcome To Company-IT!", "Explore US!!", "Happy To Help You!!!"]}
-                        typingSpeed={75}
-                        pauseDuration={1500}
-                        showCursor={true}
-                        cursorCharacter="●"
-                    />
-                    <br />
-                    <div className='grid grid-flow-col grid-rows-3 gap-4'>
-                        <div className='pt-5 break-all row-span-3'>
-                        <div className='text-xl'
-                            ref={containerRef}
-                            style={{ position: 'relative' }}
-                        >
-
-                            <VariableProximity
-                                label={description}
-                                className={'variable-proximity-demo' + 'text-wrap pt-4 md:pl-20 md:pr-20 text-white'}
-                                fromFontVariationSettings="'wght' 400, 'opsz' 9"
-                                toFontVariationSettings="'wght' 1000, 'opsz' 40"
-                                containerRef={containerRef}
-                                radius={100}
-                                falloff='linear'
-                            />
-
-                        </div>
-                    </div>
-                    <div className='col-span-45'>
-                        
-                    </div>
-                    </div>
-                    
-                </div>
-
-
-            </div>
-</ClickSpark>
-
-           
+<section className="bg-[url()] bg-fixed px-2 sm:px-10 from-[#F5F7FF] via-[#fffbee] to-[#E6EFFF] pt-6 h-full">
+    <main className="flex-grow flex flex-col items-center max-w-7xl mx-auto w-full">
+        <button
+            className="mt-16 mb-6 flex items-center space-x-2 border border-indigo-600 text-indigo-600 text-xs rounded-full px-4 pr-1.5 py-1.5 hover:bg-indigo-50 transition"
+            type="button">
+            <span>
+                Explore how we help grow brands.
+            </span>
+            <span className="flex items-center justify-center size-6 p-1 rounded-full bg-indigo-600">
+                <svg width="14" height="11" viewBox="0 0 16 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M1 6.5h14M9.5 1 15 6.5 9.5 12" stroke="#fff" stroke-width="1.5" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </svg>
+            </span>
+        </button>
+        <TextType className=' md:text-6xl sm:text-4xl font-bold text-black pb-3'
+            text={["Welcome To Company-IT!", "Happy To Help You!", "Your Success, Our Commitment!"]}
+            typingSpeed={75}
+            pauseDuration={1500}
+            showCursor={true}
+            cursorCharacter="●"
+        />
+        <h1 className="text-center text-gray-900 font-semibold text-3xl sm:text-4xl md:text-5xl max-w-2xl leading-tight">
+            Preferred choice of leaders in 
+            <span className="text-indigo-600">
+                 every industry
+            </span>
+        </h1>
+        <p className="mt-4 text-center text-gray-600 max-w-md text-sm sm:text-base leading-relaxed">
+            Learn why professionals trust our solution to complete
+            their customer journey.
+        </p>
+        <button
+            className="mt-8 bg-indigo-600 text-white px-6 pr-2.5 py-2.5 rounded-full text-sm font-medium flex items-center space-x-2 hover:bg-indigo-700 transition"
+            type="button">
+            <span>
+                Read Success Stories
+            </span>
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4.821 11.999h13.43m0 0-6.714-6.715m6.715 6.715-6.715 6.715" stroke="#fff"
+                    stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round" />
+            </svg>
+        </button>
+        <div aria-label="Photos of leaders" className="mt-12 flex max-md:overflow-x-auto gap-6 max-w-4xl w-full pb-6">
+            <img alt=""
+                className="w-36 h-44 hover:-translate-y-1 transition duration-300 rounded-lg object-cover flex-shrink-0"
+                height="140"
+                src="https://images.unsplash.com/flagged/photo-1573740144655-bbb6e88fb18a?q=80&w=735&auto=format&fit=crop"
+                width="120" />
+            <img alt=""
+                className="w-36 h-44 hover:-translate-y-1 transition duration-300 rounded-lg object-cover flex-shrink-0"
+                height="140"
+                src="https://images.unsplash.com/photo-1573497019940-1c28c88b4f3e?q=80&w=687&auto=format&fit=crop"
+                width="120" />
+            <img alt=""
+                className="w-36 h-44 hover:-translate-y-1 transition duration-300 rounded-lg object-cover flex-shrink-0"
+                height="140"
+                src="https://images.unsplash.com/photo-1560250097-0b93528c311a?q=80&w=687&auto=format&fit=crop"
+                width="120" />
+            <img alt=""
+                className="w-36 h-44 hover:-translate-y-1 transition duration-300 rounded-lg object-cover flex-shrink-0"
+                height="140"
+                src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?q=80&w=687&auto=format&fit=crop"
+                width="120" />
+            <img alt=""
+                className="w-36 h-44 hover:-translate-y-1 transition duration-300 rounded-lg object-cover flex-shrink-0"
+                height="140"
+                src="https://images.unsplash.com/photo-1639149888905-fb39731f2e6c?q=80&w=764&auto=format&fit=crop"
+                width="120" />
         </div>
-    )
+    </main>
+</section>
+    </div>
+  )
 }
 
 export default hero
